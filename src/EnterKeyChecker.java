4a18ad4fc6f3fa123f99aec63494f656931b4f09
Mehran <ajaxcube@gmail.com>
2018-11-13 15:04:06 +0100

    Initial commit

---
+++ src/EnterKeyChecker.java
@@ -0,0 +1,20 @@
+//Check if user pressed ENTER,
+import java.io.IOException;
+public class EnterKeyChecker {
+    public void pressEnter() {
+        int count = 10;
+        System.out.println("Please press \"Enter\" to proceed");
+        int rawInput = 0;
+        try {
+            rawInput = System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        char letter = (char) count;
+        if (letter == rawInput) {
+            System.out.println("You pressed enter. You're signed up, thank you.");
+        } else {
+            System.out.println("Sorry, you did not press \"Enter\".");
+        }
+    }
+}
